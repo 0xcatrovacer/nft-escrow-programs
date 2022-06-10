a969d0eec78be1ae057632fb53a0573edbf0bfb7
Swarnab Garang <72680953+0xcatrovacer@users.noreply.github.com>
2022-06-10 23:25:08 +0530

    feat: Added CpiContext functions in Exchange impl block

--- programs/escrow-contract/src/lib.rs
+++ programs/escrow-contract/src/lib.rs
@@ -249,6 +249,41 @@ pub struct Exchange<'info> {
     pub token_program: AccountInfo<'info>,
 }
 
+impl<'info> Exchange<'info> {
+    fn transfer_to_initializer_context(&self) -> CpiContext<'_, '_, '_, 'info, Transfer<'info>> {
+        let cpi_accounts = Transfer {
+            from: self.taker_deposit_token_account.to_account_info().clone(),
+            to: self
+                .initializer_receive_token_account
+                .to_account_info()
+                .clone(),
+            authority: self.taker.clone(),
+        };
+
+        CpiContext::new(self.token_program.clone(), cpi_accounts)
+    }
+
+    fn transfer_nft_to_taker_context(&self) -> CpiContext<'_, '_, '_, 'info, Transfer<'info>> {
+        let cpi_accounts = Transfer {
+            from: self.vault_account.to_account_info().clone(),
+            to: self.taker_receive_token_account.to_account_info().clone(),
+            authority: self.vault_authority.clone(),
+        };
+
+        CpiContext::new(self.token_program.clone(), cpi_accounts)
+    }
+
+    fn close_account_context(&self) -> CpiContext<'_, '_, '_, 'info, CloseAccount<'info>> {
+        let cpi_accounts = CloseAccount {
+            account: self.vault_account.to_account_info().clone(),
+            destination: self.initializer.clone(),
+            authority: self.vault_authority.clone(),
+        };
+
+        CpiContext::new(self.token_program.clone(), cpi_accounts)
+    }
+}
+
 #[account]
 pub struct EscrowAccount {
     pub initializer_key: Pubkey,
